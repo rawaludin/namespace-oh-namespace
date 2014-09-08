1b499caf5128d7da365762526fcabb673a615344
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2014-09-08 23:05:13 +0700

    Contoh penggunaan namespace untuk menghindari duplikasi

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <?php
 // buatan kita
+namespace Gue;
 class URLShortener {
     public function __construct() {
-        echo "Membuat shortener...";
+        echo "Membuat shortener...\n";
     }
 }
 
 // dari Bitly
+namespace Bitly;
 class URLShortener {
     public function __construct() {
-        echo "Membuat shortener dengan API Bitly...";
+        echo "Membuat shortener dengan API Bitly...\n";
     }
 }
 
-$shortener = new URLShortener();
+$shortener = new \Gue\URLShortener();
+$bitly = new \Bitly\URLShortener();
